75ab9e7ff59ced26ba7d258d2af9b261f5855c2b
Sebastian Sipos <sebastiansipos90@gmail.com>
2019-11-21 21:58:35 +0200

    Add basic test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,24 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import           Network.URL                    ( URL(URL)
+                                                , URLType(Absolute)
+                                                , Host(Host)
+                                                , Protocol(HTTP)
+                                                )
+import           Test.Hspec
+import           HsMusic
+import           Protolude
+
+makeUrl :: [Char] -> URL
+makeUrl domain = URL (Absolute (Host (HTTP True) domain Nothing)) "" []
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ describe "Decode YAML as expected" $ do
+  it "Decodes and empty string to an empty array" $ do
+    let expected = Right []
+    decodeYAML "" `shouldBe` expected
+  it "Decodes one item properly" $ do
+    let playlistURL  = makeUrl "google.com"
+    let playlistName = "minimal"
+    let expected     = Right [[Playlist { .. }]]
+    shouldBe (decodeYAML "- name: minimal\n  url: https://google.com") expected
